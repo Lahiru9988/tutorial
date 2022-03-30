b44403405c27ba6a3345fd8c9ab6b650cf9dd5a0
KYLINE <72918623+Lahiru9988@users.noreply.github.com>
2022-03-30 10:57:41 +0530

    update

---
+++ app/src/main/java/com/firstapp/tutorial/Display_massage.java
@@ -0,0 +1,14 @@
+package com.firstapp.tutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Display_massage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_massage);
+    }
+}
--- app/src/main/java/com/firstapp/tutorial/MainActivity.java
+++ app/src/main/java/com/firstapp/tutorial/MainActivity.java
@@ -2,13 +2,28 @@ package com.firstapp.tutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String EXTRA_MESSAGE = "com.example.myriads.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    /** called when the user taps the send button */
+    public void sendMessage(View view ){
+        Intent intent = new Intent(this, Display_massage.class);
+        EditText editText = (EditText) findViewById(R.id.et_massage);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+
 }
